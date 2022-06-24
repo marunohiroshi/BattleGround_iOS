cf8746a5421a53a32e8c0f074df1205c15f0b225
maruno hiroshi <hiro.0405.maru@gmail.com>
2022-06-25 08:45:50 +0900

    Initial Commit

---
+++ BattleGround/BattleGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleGroundApp.swift
+//  BattleGround
+//
+//  Created by 丸野拓志 on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BattleGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BattleGround/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BattleGround
+//
+//  Created by 丸野拓志 on 2022/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
